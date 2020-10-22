7e1cf83717c65faa5a303808510a6756c151e597
Espen <EspenBG>
2020-10-22 14:55:34 +0200

    Fixed TODOs in README.md and added info on branches

--- ExampleCode/Eksempelkode - IELET2001 Prosjekt/Node-Socket (tjener)/server_no_encrypt.js
+++ ExampleCode/Eksempelkode - IELET2001 Prosjekt/Node-Socket (tjener)/server_no_encrypt.js
@@ -42,7 +42,7 @@ io.on('connection', function(socket){ //This is the server part of the "what hap
 
     //Change states (general user defined functions)
     socket.on('changeLEDState', function(state) { //This server function constantly checks if a client (webpage) calls its
-        //If the webpage calles it it will us the "io.emit" (to send to alle clients) and not "client.emit" to only send to one client
+        //If the webpage calls it it will us the "io.emit" (to send to alle clients) and not "client.emit" to only send to one client
         //In this way, when we send it to call clients, the ESP32 will get the message. It is an easy solution which can be made better
 
         io.emit('LEDStateChange', state); //This is the actual socket.io emit function
