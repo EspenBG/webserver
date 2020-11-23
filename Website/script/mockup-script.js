5e373f7ae5104cc8ee9c6495a86870ea3a8226bb
espen <EspenBG>
2020-11-23 16:00:39 +0100

    Started on the script for getting data form the server

--- Website/script/mockup-script.js
+++ Website/script/mockup-script.js
@@ -1,5 +1,5 @@
-const serverIP = 'http://localhost:3000';
-const websiteNamespace = '/webserver';
+//const serverIP = 'http://localhost:3000';
+//const websiteNamespace = '/webserver';
 
 
 const socket = io('http://localhost:3000/webserver', {
@@ -10,9 +10,25 @@ const socket = io('http://localhost:3000/webserver', {
 socket.on('connect', ()=> {
     console.log(socket.id);
     console.log(socket.nsp);
+    console.log('Get all sensor info');
+    getAllSensors();
+    console.log('Get sensor info for sensor #####1')
+    getSensorInfo('#####1');
 })
 
 // TODO:Get all sensors that is on the robot server
+function getAllSensors () {
+    socket.emit('allSensors', true);
+}
+socket.on('allSensors', (sensors) => {
+    console.log(sensors);
+});
+function getSensorInfo (sensor) {
+    socket.emit('sensorInfo', sensor);
+}
+socket.on('sensorInfo', (sensorInfo) => {
+   console.log(sensorInfo);
+});
 
 // TODO: Make function to get sensor data form robotserver
 
