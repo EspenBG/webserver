894c537e5f2a4b275b5d8cafa0df3e400a4ac038
EspenBG <espen.godo@gmail.com>
2020-12-18 10:24:21 +0100

    Wip/forever test (#6)
    * Config file for forever
    
    * Config file for forever
    
    * Refactoring of the website is finished.
    - The scripts has not been refactored yet.
    
    * All refactoring is now finished.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /***********************************************************************************************************************
  * WEBSERVER
- * THIS IS A HTTP SERVER FOR PROCESSING HTTP REQUESTS
+ * THIS IS A PROGRAM FOR SUPPLYING DOCUMENTS FOR WEBCLIENTS
  * WRITTEN AS A PART OF THE SUBJECT IELEA2001
  ***********************************************************************************************************************/
 
@@ -17,14 +17,15 @@ const server = require('http').createServer(app);
 * MAIN PROGRAM
 *********************************************************************/
 // Set the directory the webclients can access, the clients can only request files from this directory
-app.use(express.static(__dirname + '/../Website'));
+app.use(express.static('/home/server/webserver/Website'));
 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.listen(8000, () => {
+// Start the http server on port 8000
+app.listen(8000, (server) => {
     console.log('Express app listening at http://localhost:8000');
 })
 
